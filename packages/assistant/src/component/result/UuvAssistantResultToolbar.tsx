28824094a66e4e41bb6289baff1ddbe0eecf9ee1
Louis Fredice NJAKO MOLOM <louisfredice.njakomolom@make-it-soft.com>
2025-08-07 00:14:26 +0200

    feat(assistant): plug with uuv ai image classifier

--- packages/assistant/src/component/result/UuvAssistantResultToolbar.tsx
+++ packages/assistant/src/component/result/UuvAssistantResultToolbar.tsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { Button, Flex, Tooltip } from "antd";
-import { CopyOutlined } from "@ant-design/icons";
+import { BulbOutlined, CopyOutlined } from "@ant-design/icons";
 
 interface UuvAssistantResultToolbarProps {
   generatedScript: string;
   copyResult: () => void;
+  onAiClick: () => void;
+  selectedElement: HTMLElement | undefined;
   getAsideParentInHierarchy: (triggerNode: HTMLElement) => HTMLElement;
 }
 
 export const UuvAssistantResultToolbar: React.FC<UuvAssistantResultToolbarProps> = ({
   generatedScript,
   copyResult,
+  onAiClick,
+  selectedElement,
   getAsideParentInHierarchy,
 }) => (
   <div id="toolbar">
-    <Flex justify="space-between" align="center">
+    <Flex justify={"start"} align={"center"} gap={20}>
       <Tooltip
         placement="bottom"
         title="Copy"
@@ -31,6 +35,23 @@ export const UuvAssistantResultToolbar: React.FC<UuvAssistantResultToolbarProps>
           onClick={copyResult}
         />
       </Tooltip>
+      { selectedElement?.tagName === "IMG" &&
+          <Tooltip
+              placement="bottom"
+              title="AI analysis"
+              getPopupContainer={(triggerNode) =>
+                  getAsideParentInHierarchy(triggerNode)
+              }
+          >
+            <Button
+                type="link"
+                shape="circle"
+                icon={<BulbOutlined />}
+                className="primary"
+                onClick={onAiClick}
+            />
+          </Tooltip>
+      }
     </Flex>
   </div>
 );
