d6d0793ce7097ba50672d007be4e252adfed0482
Louis Fredice NJAKO MOLOM <louisfredice.njakomolom@make-it-soft.com>
2025-08-15 19:18:34 +0200

    feat: consume ai api as stream

--- packages/assistant/src/component/result/UuvAssistantResultAIAnalysis.tsx
+++ packages/assistant/src/component/result/UuvAssistantResultAIAnalysis.tsx
@@ -1,27 +1,72 @@
 import React from "react";
 import { Descriptions, Skeleton } from "antd";
+import { PENDING_VALUE, UuvAssistantResultAIAnalysisType } from "../../Commons";
 
 interface UuvAssistantResultAIAnalysisProps {
-  aiResult: any | "pending" | undefined;
+  aiResult: UuvAssistantResultAIAnalysisType;
 }
 
 export const UuvAssistantResultAIAnalysis: React.FC<UuvAssistantResultAIAnalysisProps> = ({
     aiResult,
 }) => {
-    const renderContent = (content: React.ReactNode) => {
-        return aiResult === "pending" ? (
-            <Skeleton className='uuv-assistant-result-item' title={false} active paragraph={{ rows: 1 }} />
-        ) : content;
+    const renderLoadingField = (fieldValue: any, finalContent: React.ReactNode, numberOfRows: number) => {
+        return fieldValue === undefined || fieldValue === null || fieldValue === PENDING_VALUE ? (
+            <Skeleton className='uuv-assistant-result-item' title={false} active paragraph={{ rows: numberOfRows }} />
+        ) : finalContent;
+    };
+
+    const formatConfidence = (confidence?: number | typeof PENDING_VALUE) => {
+        if(!confidence || typeof confidence !== 'number') {
+            return 'N/A';
+        }
+        return `${(confidence * 100).toFixed(2)}%`;
+    };
+
+    const formatClassification = (is_decorative?: boolean | typeof PENDING_VALUE) => {
+        if(is_decorative === undefined || is_decorative === null) {
+            return 'N/A';
+        }
+        return is_decorative ? 'Decorative' : 'Informative';
     };
 
     return (
         <div id="aiResult">
             <Descriptions id="aiResultDescription" title="AI image analysis">
-                <Descriptions.Item label="Is decorative">{renderContent(String(aiResult?.is_decorative))}</Descriptions.Item>
-                <Descriptions.Item label="Confidence">{renderContent(aiResult?.confidence)}</Descriptions.Item>
-                <Descriptions.Item label="Duration">{renderContent(aiResult?.duration + 's')}</Descriptions.Item>
-                <Descriptions.Item label="Image description" span={3}>{renderContent(aiResult?.image_description)}</Descriptions.Item>
-                <Descriptions.Item label="Analyse details" span={3}>{renderContent(aiResult?.analysis_details)}</Descriptions.Item>
+                <Descriptions.Item label="Classification">{
+                    renderLoadingField(
+                        aiResult?.is_decorative,
+                        formatClassification(aiResult?.is_decorative),
+                        1
+                    )
+                }</Descriptions.Item>
+                <Descriptions.Item label="Confidence">{
+                    renderLoadingField(
+                        aiResult?.confidence,
+                        formatConfidence(aiResult?.confidence),
+                        1
+                    )
+                }</Descriptions.Item>
+                <Descriptions.Item label="Duration">{
+                    renderLoadingField(
+                        aiResult?.duration,
+                        aiResult?.duration + 's',
+                        1
+                    )
+                }</Descriptions.Item>
+                <Descriptions.Item label="Image description" span={3}>{
+                    renderLoadingField(
+                        aiResult?.image_description,
+                        aiResult?.image_description,
+                        2
+                    )
+                }</Descriptions.Item>
+                <Descriptions.Item label="Analyse details" span={3}>{
+                    renderLoadingField(
+                        aiResult?.analysis_details,
+                        aiResult?.analysis_details,
+                        2
+                    )
+                }</Descriptions.Item>
             </Descriptions>
         </div>
     );
