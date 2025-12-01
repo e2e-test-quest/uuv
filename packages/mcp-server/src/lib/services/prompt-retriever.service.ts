8d5562182abb70c7bdc8d653eb52fe99afdea941
Louis Fredice NJAKO MOLOM <louisfredice.njakomolom@make-it-soft.com>
2025-12-01 22:11:24 +0100

    feat(mcp-server): add mcp prompts, #1171

--- packages/mcp-server/src/lib/services/prompt-retriever.service.ts
+++ packages/mcp-server/src/lib/services/prompt-retriever.service.ts
@@ -18,6 +18,65 @@ export enum UUV_PROMPT {
     GENERATE_TEST_TYPE_ELEMENT = "generate_test_type_element"
 }
 
+export const uuvPrompts = {
+    [UUV_PROMPT.GENERATE_TEST_EXPECT_TABLE]: {
+        title: "GenerateTestExpectTable",
+        description: "Prompt to generate test for html table or grid or treeGrid"
+    },
+    [UUV_PROMPT.GENERATE_TEST_EXPECT_ELEMENT]: {
+        title: "GenerateTestExpectElement",
+        description: "Prompt to generate test that expects of html element with (role and name) or domSelector"
+    },
+    [UUV_PROMPT.GENERATE_TEST_CLICK_ELEMENT]: {
+        title: "GenerateTestClickElement",
+        description: "Prompt to generate test that clicks on html element with (role and name) or domSelector"
+    },
+    [UUV_PROMPT.GENERATE_TEST_WITHIN_ELEMENT]: {
+        title: "GenerateTestWithinElement",
+        description: "Prompt to generate test that focus within an html element with (role and name) or domSelector"
+    },
+    [UUV_PROMPT.GENERATE_TEST_TYPE_ELEMENT]: {
+        title: "GenerateTestTypeElement",
+        description: "Prompt to generate test that types a value into html element with (role and name) or domSelector"
+    }
+};
+
+export const UUVPromptInputSchema = z.discriminatedUnion("promptName", [
+    z.object({
+        promptName: z.literal(UUV_PROMPT.GENERATE_TEST_EXPECT_TABLE),
+        baseUrl: z.string().describe("The base URL of the page where the table/grid/treegrid is located."),
+    }),
+    z.object({
+        promptName: z.enum([
+            UUV_PROMPT.GENERATE_TEST_EXPECT_ELEMENT,
+            UUV_PROMPT.GENERATE_TEST_CLICK_ELEMENT,
+            UUV_PROMPT.GENERATE_TEST_WITHIN_ELEMENT,
+            UUV_PROMPT.GENERATE_TEST_TYPE_ELEMENT
+        ]),
+        baseUrl: z.string().describe("The base URL of the page where the element is located."),
+        accessibleName: z.string().optional().describe("Accessible name of the element"),
+        accessibleRole: z.string().optional().describe("Accessible role of the element"),
+        domSelector: z.string().optional().describe("Dom selector of the element"),
+    }),
+]).superRefine((data, ctx) => {
+    if (
+        data.promptName === UUV_PROMPT.GENERATE_TEST_EXPECT_ELEMENT ||
+        data.promptName === UUV_PROMPT.GENERATE_TEST_CLICK_ELEMENT  ||
+        data.promptName === UUV_PROMPT.GENERATE_TEST_WITHIN_ELEMENT ||
+        data.promptName === UUV_PROMPT.GENERATE_TEST_TYPE_ELEMENT
+    ) {
+        const hasAccessibleSelector = data.accessibleRole && data.accessibleName;
+        const hasDomSelector = data.domSelector;
+
+        if (!hasAccessibleSelector && !hasDomSelector) {
+            ctx.addIssue({
+                code: z.ZodIssueCode.custom,
+                message: "You must provide either (accessibleRole AND accessibleName) or domSelector",
+            });
+        }
+    }
+});
+
 
 export class PromptRetrieverService {
     private static loadPromptTemplate(promptName: string): string {
@@ -40,16 +99,30 @@ export class PromptRetrieverService {
                 promptName: z.enum([
                     UUV_PROMPT.GENERATE_TEST_EXPECT_ELEMENT,
                     UUV_PROMPT.GENERATE_TEST_CLICK_ELEMENT,
-                    UUV_PROMPT.GENERATE_TEST_WITHIN_ELEMENT,
-                    UUV_PROMPT.GENERATE_TEST_TYPE_ELEMENT
+                    UUV_PROMPT.GENERATE_TEST_WITHIN_ELEMENT
                 ]),
                 baseUrl: z.string().describe("The base URL of the page where the element is located."),
                 accessibleName: z.string().optional().describe("Accessible name of the element"),
                 accessibleRole: z.string().optional().describe("Accessible role of the element"),
                 domSelector: z.string().optional().describe("Dom selector of the element"),
             }),
+            z.object({
+                promptName: z.enum([
+                    UUV_PROMPT.GENERATE_TEST_TYPE_ELEMENT
+                ]),
+                baseUrl: z.string().describe("The base URL of the page where the element is located."),
+                valueToType: z.string().describe("Value to type"),
+                accessibleName: z.string().optional().describe("Accessible name of the element"),
+                accessibleRole: z.string().optional().describe("Accessible role of the element"),
+                domSelector: z.string().optional().describe("Dom selector of the element"),
+            })
         ]).superRefine((data, ctx) => {
-            if (data.promptName === UUV_PROMPT.GENERATE_TEST_EXPECT_ELEMENT || data.promptName === UUV_PROMPT.GENERATE_TEST_CLICK_ELEMENT) {
+            if (
+                data.promptName === UUV_PROMPT.GENERATE_TEST_EXPECT_ELEMENT ||
+                data.promptName === UUV_PROMPT.GENERATE_TEST_CLICK_ELEMENT  ||
+                data.promptName === UUV_PROMPT.GENERATE_TEST_WITHIN_ELEMENT ||
+                data.promptName === UUV_PROMPT.GENERATE_TEST_TYPE_ELEMENT
+            ) {
                 const hasAccessibleSelector = data.accessibleRole && data.accessibleName;
                 const hasDomSelector = data.domSelector;
 
