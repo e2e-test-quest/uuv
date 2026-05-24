193bb168c75c17abcdc5a9094aed091953ec156f
Louis Fredice NJAKO MOLOM <louisfredice.njakomolom@make-it-soft.com>
2026-05-24 21:57:40 +0200

    docs(docs): add mcp-server badge

--- packages/docs/src/pages/index.tsx
+++ packages/docs/src/pages/index.tsx
@@ -11,52 +11,68 @@ import styles from './index.module.css';
 function HomepageHeader() {
     const {siteConfig} = useDocusaurusContext();
     return (
-        <header className={clsx('hero', styles.heroBanner)}>
+        <header className={clsx("hero", styles.heroBanner)}>
             <div className="container">
-                <img className="hero__logo" src="img/uuv.png" height="150px"/>
+                <img className="hero__logo" src="img/uuv.png" height="150px" />
                 <h1 className="hero__title">{siteConfig.title}</h1>
                 <h3>User centric Usecases Validator</h3>
-                <p className="hero__subtitle">{translate({
-                    id: 'homepage.headline',
-                    message: siteConfig.tagline
-                })}</p>
+                <p className="hero__subtitle">
+                    {translate({
+                        id: "homepage.headline",
+                        message: siteConfig.tagline,
+                    })}
+                </p>
                 <div className={styles.homepageShortDescription}>
                     <p className="">
-                        <Translate id="homepage.shortDescription">An accessibility driven solution
-                            to facilitate the writing and execution of end-to-end tests that are understandable to any human being.</Translate>
+                        <Translate id="homepage.shortDescription">
+                            An accessibility driven solution to facilitate the writing and execution of end-to-end tests that are understandable to
+                            any human being.
+                        </Translate>
                     </p>
                 </div>
                 <div className={styles.downloadSection}>
-                    <a href={'https://www.npmjs.com/package/@uuv/cypress'} target={'_blank'}>
-                        <img alt="@uuv/cypress npm library download count"
-                             src="https://img.shields.io/npm/dt/%40uuv/cypress?logo=npm&label=%40uuv%2Fcypress"></img>
+                    <a href={"https://www.npmjs.com/package/@uuv/cypress"} target={"_blank"}>
+                        <img
+                            alt="@uuv/cypress npm library download count"
+                            src="https://img.shields.io/npm/dt/%40uuv/cypress?logo=npm&label=%40uuv%2Fcypress"
+                        ></img>
                     </a>
-                    <a href={'https://www.npmjs.com/package/@uuv/playwright'} target={'_blank'}>
-                        <img alt="@uuv/playwright npm library download count"
-                             src="https://img.shields.io/npm/dt/%40uuv/playwright?logo=npm&label=%40uuv%2Fplaywright"></img>
+                    <a href={"https://www.npmjs.com/package/@uuv/playwright"} target={"_blank"}>
+                        <img
+                            alt="@uuv/playwright npm library download count"
+                            src="https://img.shields.io/npm/dt/%40uuv/playwright?logo=npm&label=%40uuv%2Fplaywright"
+                        ></img>
                     </a>
-                    <a href={'https://www.npmjs.com/package/@uuv/assistant'} target={'_blank'}>
-                        <img alt="@uuv/assistant npm library download count"
-                             src="https://img.shields.io/npm/dt/%40uuv/assistant?logo=npm&label=%40uuv%2Fassistant"></img>
+                    <a href={"https://www.npmjs.com/package/@uuv/assistant"} target={"_blank"}>
+                        <img
+                            alt="@uuv/assistant npm library download count"
+                            src="https://img.shields.io/npm/dt/%40uuv/assistant?logo=npm&label=%40uuv%2Fassistant"
+                        ></img>
                     </a>
-                    <a href={'https://www.npmjs.com/package/@uuv/a11y'} target={'_blank'}>
-                        <img alt="@uuv/a11y npm library download count"
-                             src="https://img.shields.io/npm/dt/%40uuv/a11y?logo=npm&label=%40uuv%2Fa11y"></img>
+                    <a href={"https://www.npmjs.com/package/@mcp-server"} target={"_blank"}>
+                        <img
+                            alt="@uuv/mcp-server npm library download count"
+                            src="https://img.shields.io/npm/dt/%40uuv/mcp-server?logo=npm&label=%40uuv%2Fmcp-server"
+                        ></img>
                     </a>
-                    <a href={'https://plugins.jetbrains.com/plugin/22437-uuv'} target={'_blank'}>
-                        <img alt="JetBrains Plugin Downloads" src="https://img.shields.io/jetbrains/plugin/d/22437-uuv?logo=jetbrains&label=UUV%20plugin"></img>
+                    <a href={"https://plugins.jetbrains.com/plugin/22437-uuv"} target={"_blank"}>
+                        <img
+                            alt="JetBrains Plugin Downloads"
+                            src="https://img.shields.io/jetbrains/plugin/d/22437-uuv?logo=jetbrains&label=UUV%20plugin"
+                        ></img>
                     </a>
-                    <a href={'https://marketplace.visualstudio.com/items?itemName=e2e-test-quest.uuv-vscode-extension'} target={'_blank'}>
-                        <img alt="VS Code Extension Downloads" src="https://img.shields.io/visual-studio-marketplace/d/e2e-test-quest.uuv-vscode-extension?label=UUV%20VS%20Code%20Extension&cacheSeconds=0"></img>
+                    <a href={"https://marketplace.visualstudio.com/items?itemName=e2e-test-quest.uuv-vscode-extension"} target={"_blank"}>
+                        <img
+                            alt="VS Code Extension Downloads"
+                            src="https://img.shields.io/visual-studio-marketplace/d/e2e-test-quest.uuv-vscode-extension?label=UUV%20VS%20Code%20Extension&cacheSeconds=0"
+                        ></img>
                     </a>
-                    <a href={'https://hub.docker.com/r/e2etesting/uuv'} target={'_blank'}>
+                    <a href={"https://hub.docker.com/r/e2etesting/uuv"} target={"_blank"}>
                         <img alt="Docker pulls" src="https://img.shields.io/docker/pulls/e2etesting/uuv?logo=docker&cacheSeconds=0"></img>
                     </a>
                 </div>
                 <div className={styles.buttons}>
-                    <Link
-                        className={`${styles.button} button button--primary button--lg`}
-                        to="docs/intro">
+                    <Link className={`${styles.button} button button--primary button--lg`} to="docs/intro">
                         <div className={styles.buttonIconWrapper}>
                             <span className={styles.buttonIconGetStarted}></span>
                         </div>
